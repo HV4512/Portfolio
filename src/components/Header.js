729ef8ff167a873561ec40ae73f01876cb28bb1f
Harsh Vardhan <jobbykarn@gmail.com>
2023-08-16 03:37:17 +0530

    1

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import name1 from '../assets/name1.svg';
+
+const Header = () => {
+  return (
+    <header className='py-8'>
+    <div className='container mx-auto'>
+    <div className='flex justify-between items-center'>
+    <a href='#'>
+
+    <img src={name1} alt='' />
+    {/*<img src={Harsh} alt='' />
+  <img src={Vardhan} alt='' />*/}
+    </a>
+    <button className='btn btn-sm'>Work with me</button>
+    </div>
+    </div>
+    </header>
+  );
+};
+
+export default Header;
