e8da854d27d78a9f9ce8f7945963c041a3e7c1ae
Harsh Vardhan <jobbykarn@gmail.com>
2023-08-16 19:42:46 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import Image from '../assets/avatar.svg';
 
-import {FaGithub, FaYoutube, FaDribble} from 'react-icons/fa';
+import {FaGithub, FaLinkedin, FaInstagram} from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
 import {motion} from 'framer-motion';
 import {fadeIn} from '../variants'; 
 
 const Banner = () => {
   return (
-    <section className='section' id='home'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
     <div className='container mx-auto'>
-    <div>
-    <div>
-    <h1>HARSH <span>VARDHAN</span></h1>
-    <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+    <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+    <div className='flex-1 text-center font-secondary lg:text-left'>
+    <motion.h1 
+    variants={fadeIn('up',0.3)}
+    initial='hidden'
+    whileInView={'show'}
+    viewport={{once: false, anount:0.7}}
+    className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+    HARSH <span>VARDHAN</span></motion.h1>
+    <motion.div 
+    variants={fadeIn('up',0.4)}
+    initial='hidden'
+    whileInView={'show'}
+    viewport={{once: false, anount:0.7}}
+    className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
     <span className='text-white mr-4'>I am a</span>
     <TypeAnimation 
     sequence={[
@@ -29,20 +40,35 @@ const Banner = () => {
     wrapper='span'
     repeat={Infinity}
     />
-    </div>
-    <p> Welcome to my portfolio, here you can find what I know and decide if you like me.
-    </p> 
-    <div>
+    </motion.div>
+    <motion.p 
+    variants={fadeIn('up',0.5)}
+    initial='hidden'
+    whileInView={'show'}
+    viewport={{once: false, anount:0.7}}
+    className='mb-8 max-w-lg mx-auto lg:mx-0'> Welcome to my portfolio, here you can find what I know and decide if you like me.
+    </motion.p> 
+    <motion.div 
+    variants={fadeIn('up',0.6)}
+    initial='hidden'
+    whileInView={'show'}
+    viewport={{once: false, anount:0.7}}
+    className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
     <button className='btn btn-lg'>Contact Me</button>
     <a href='#' className='text-gradient btn-link'>My Portfolio</a>
+    </motion.div>
+    {/* Socials */}
+   <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+   <a href='#'><FaGithub/></a>
+   <a href='#'><FaLinkedin/></a>
+   <a href='#'><FaInstagram/></a>
+   </div>
     </div>
-    </div>
-    <div> 
+    <div className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'> {/* HeadShot Image */}
     <img src={Image} alt=''/>
     </div>
     </div>
     </div>
-    Banner
     </section>
   );
 };
