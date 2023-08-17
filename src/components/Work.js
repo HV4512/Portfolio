a8b5c202ba3648b0dc93da780a2fe3e2951037e4
Harsh Vardhan <jobbykarn@gmail.com>
2023-08-17 22:20:20 +0530

    init-1

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,83 @@
 import React from 'react';
 
+import { motion } from 'framer-motion';
+
+import { fadeIn } from '../variants';
+import Img1 from '../assets/portfolio-img1.png';
+import Img2 from '../assets/portfolio-img2.png';
+import Img3 from '../assets/portfolio-img3.png';
+
 const Work = () => {
-  return <div className='section' id='work'>Work</div>;
+  return (<section className='section' id='work'>
+    <div className='container mx-auto'>
+      <div className='flex flex-col lg:flex-row gap-x-10'>
+        <motion.div 
+        variants={fadeIn('right',0.3)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false, amount:0.3}}
+        className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+          {/* text */}
+          <div>
+            <h2 className='h2 leading-tight  text-accent'>
+              My Latest<br/>Work
+            </h2>
+            <p className='max-w-sm mb-16'>Et exercitation commodo quis exercitation magna magna proident minim laborum id ad eiusmod incididunt.</p>
+            <button className='btn btn-sm'>View All Projects</button>
+          </div>
+          {/* Image1*/}
+          <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+            {/* Overlay */}
+            <div className='group-hover:bg-black/60 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt=''/>
+            {/* Pretitle */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 '>
+              <span className='text-gradient'>UI/UX DESIGN</span>
+              </div>
+            {/* title */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>Project Title</span>
+            </div>
+          </div>
+        </motion.div>
+        <motion.div 
+        variants={fadeIn('left',0.2)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once:false, amount:0.3}}
+        className='flex-1 flex flex-col gap-y-10'>
+          {/* Image 2*/}
+        <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+            {/* Overlay */}
+            <div className='group-hover:bg-black/60 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt=''/>
+            {/* Pretitle */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 '>
+              <span className='text-gradient'>UI/UX DESIGN</span>
+              </div>
+            {/* title */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>Project Title</span>
+            </div>
+          </div>
+          {/* Image 3 */}
+        <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+            {/* Overlay */}
+            <div className='group-hover:bg-black/60 w-full h-full absolute z-40 transition-all duration-300'></div>
+            <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt=''/>
+            {/* Pretitle */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50 '>
+              <span className='text-gradient'>UI/UX DESIGN</span>
+              </div>
+            {/* title */}
+            <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+              <span className='text-3xl text-white'>Project Title</span>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+    </section>);
 };
 
 export default Work;
